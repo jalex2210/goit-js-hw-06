f5aa676bceef5d3cc97b7b137b9345f4702cec98
jalex2210 <alexbenitez.rdm@gmail.com>
2023-09-25 17:34:25 -0500

    task 2

--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,13 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+const ingredientsList = document.getElementById('ingredients');
+const piece = document.createDocumentFragment();
+ingredients.forEach(ingredient => {
+  const listItem = document.createElement('li');
+  listItem.textContent = ingredient;
+  listItem.classList.add('item');
+  piece.appendChild(listItem);
+});
+  ingredientsList.appendChild(piece);
